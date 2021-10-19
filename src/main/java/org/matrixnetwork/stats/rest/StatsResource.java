47218aa38d13c67e8a4d0124c918f95d5ef475c6
Darius <pavelescu_darius@yahoo.com>
2021-10-19 17:08:00 +0200

    Implemented /api/stats/<username> returning basic stats

--- src/main/java/org/matrixnetwork/stats/rest/StatsResource.java
+++ src/main/java/org/matrixnetwork/stats/rest/StatsResource.java
@@ -1,17 +1,35 @@
 package org.matrixnetwork.stats.rest;
 
+import org.bukkit.entity.Player;
+import org.json.simple.JSONObject;
+import org.matrixnetwork.stats.MatrixStats;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/test")
+@Path("/stats")
 public class StatsResource {
 
+    @Path("/{username}")
     @GET
-    @Produces( MediaType.TEXT_PLAIN )
-    public Response meet() {
-        return Response.ok("Hello").build();
+    @Produces( MediaType.APPLICATION_JSON )
+    public Response meet(@PathParam("username") String username) {
+        if(MatrixStats.getPlugin().getServer().getPlayer(username) != null) {
+            Player p = MatrixStats.getPlugin().getServer().getPlayer(username);
+            JSONObject obj = new JSONObject();
+            obj.put("exp", p.getExp());
+            obj.put("food_level", p.getFoodLevel());
+            obj.put("loc_x", p.getLocation().getBlockX());
+            obj.put("loc_y", p.getLocation().getBlockY());
+            obj.put("loc_z", p.getLocation().getBlockZ());
+            return Response.ok(obj.toJSONString()).build();
+        }
+        else {
+            return Response.status(404).build();
+        }
     }
 }
