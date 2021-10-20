6a92d5eca5f3ceccbb2a9bcf1462d865e2154836
Darius <pavelescu_darius@yahoo.com>
2021-10-20 16:15:28 +0200

    tested and fixed auth system

--- src/main/java/org/matrixnetwork/stats/rest/AuthResource.java
+++ src/main/java/org/matrixnetwork/stats/rest/AuthResource.java
@@ -4,13 +4,17 @@ import fr.xephi.authme.api.v3.AuthMeApi;
 import org.jboss.resteasy.annotations.Form;
 import org.json.simple.JSONObject;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/auth")
 public class AuthResource {
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @POST
     public Response login(@FormParam("username") String username, @FormParam("password") String password) {
         if(AuthMeApi.getInstance().checkPassword(username, password)) {
